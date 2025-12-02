37afd0bbbab3e21d137afc0aae61e95e541813b3
Oleh <roadto12mmr@gmail.com>
2025-12-02 15:48:06 +0100

    bug fixed

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
 app.use(express.static(path.join(__dirname, 'public')));
 
